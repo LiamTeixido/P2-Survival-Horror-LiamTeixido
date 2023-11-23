81ad50c00cd05169430a88a9a853c45ce44de6d4
LiamTeixido <liamteixido@gmail.com>
2023-11-23 17:28:13 -0500

    Corrección de cámara de Player - Liam Teixido

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
-    private Rigidbody rb;
 
     public float horizontalInput;
     public float verticalInput;
 
+    private CharacterController player;
+    public float movementSpeed = 5.0f;
+
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        player = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        rb.velocity = new Vector3(
-            PlayerInput.Instance.DirectionalInput.x, rb.velocity.y,
-            PlayerInput.Instance.DirectionalInput.y) * speed;
+        float forwardMovement = Input.GetAxis("Vertical") * movementSpeed;
+        float strafeMovement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        Vector3 speed = new Vector3(strafeMovement, 0, forwardMovement);
+        speed = transform.rotation * speed;
+        player.SimpleMove(speed);
     }
 }
