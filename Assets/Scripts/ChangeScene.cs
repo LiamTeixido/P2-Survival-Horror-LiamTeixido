18ff9aab47ca5e001939709a17dfd72920ec2150
LiamTeixido <liamteixido@gmail.com>
2023-11-23 01:50:13 -0500

    Se agregó el plugin de Gamejolt - Liam Teixido
    Se agregó el logro de login

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GameJolt.UI;
+using GameJolt.API;
 
 public class ChangeScene : MonoBehaviour
 {
     public void ChangeScenes(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+
+        if (SceneManager.GetActiveScene().name == "Trophies")
+        {
+            GameJoltUI.Instance.ShowTrophies();
+        }
     }
 }
